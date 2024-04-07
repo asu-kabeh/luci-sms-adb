5bd6c61220622e5e11fb6dfc7a57be3c7e10f9ba
bolone-sengkuni <toyib30071999@gmail.com>
2024-04-07 19:46:02 +0700

    c

--- packages/luci-sms-adb/root/www/sms_adb/sms_adb.php
+++ packages/luci-sms-adb/root/www/sms_adb/sms_adb.php
@@ -1,44 +1,56 @@
 <?php
 $shell = 'adb shell content query --uri content://sms --projection _id,address,date,body';
-$excec_adb = explode("\n", shell_exec($shell));
+$excec_adb = shell_exec($shell);
 
-$maxData = 20;
-$count = 0;
+if ($excec_adb !== null) {
+    $excec_adb = explode("\n", $excec_adb);
 
-$result = array();
-foreach ($excec_adb as $line) {
-    $asu = explode("=", $line);
-    if (isset($asu[2]) && isset($asu[3]) && isset($asu[4])) {
-        $addressParts = explode(",", $asu[2]);
-        $dateParts = explode(",", $asu[3]);
-        $pesanParts = explode(",", $asu[4]);
-        if (isset($addressParts[0]) && isset($dateParts[0]) && isset($pesanParts[0])) {
-            $address = $addressParts[0];
-            $date = (int) ($dateParts[0] / 1000);
-            $dateTime = new DateTime();
-            $dateTime->setTimestamp($date);
-            $pesan = $pesanParts[0];
-            $result[] = array(
-                'date' => $dateTime->format('Y-m-d H:i:s'),
-                'address' => $address,
-                'pesan' => $pesan
-            );
+    $maxData = 20;
+    $count = 0;
 
-            $count++;
-            if ($count >= $maxData) {
-                break;
+    $result = array();
+    foreach ($excec_adb as $line) {
+        try {
+            $asu = explode("=", $line);
+            if (isset($asu[2]) && isset($asu[3]) && isset($asu[4])) {
+                $addressParts = explode(",", $asu[2]);
+                $dateParts = explode(",", $asu[3]);
+                $pesanParts = explode(",", $asu[4]);
+                if (isset($addressParts[0]) && isset($dateParts[0]) && isset($pesanParts[0])) {
+                    $address = $addressParts[0];
+                    $date = (int)($dateParts[0] / 1000);
+                    $dateTime = new DateTime();
+                    $dateTime->setTimestamp($date);
+                    $pesan = $pesanParts[0];
+                    $result[] = array(
+                        'date' => $dateTime->format('Y-m-d H:i:s'),
+                        'address' => $address,
+                        'pesan' => $pesan
+                    );
+
+                    $count++;
+                    if ($count >= $maxData) {
+                        break;
+                    }
+                }
             }
+        } catch (Exception $e) {
+            echo 'Kesalahan: ' . $e->getMessage();
         }
     }
-}
 
-$html = '';
-foreach ($result as $sms) {
-    $html .= "<tr>";
-    $html .= "<td>{$sms['date']}</td>";
-    $html .= "<td>{$sms['address']}</td>";
-    $html .= "<td>{$sms['pesan']}</td>";
-    $html .= "</tr>";
+    $html = '';
+    foreach ($result as $sms) {
+        $html .= "<tr>";
+        $html .= "<td>{$sms['date']}</td>";
+        $html .= "<td>{$sms['address']}</td>";
+        $html .= "<td>{$sms['pesan']}</td>";
+        $html .= "</tr>";
+    }
+    echo $html;
+} else {
+    $html = '';
+    $html .= "<td>ADB ERROR</td>";
+    echo $html;
 }
-echo $html;
 ?>
